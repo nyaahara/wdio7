c7c0b7d059f48711f082f798d23a1ecd57688337
nyaahara <d.niihara@gmail.com>
2021-03-21 17:33:46 +0900

    3. Add services/devicefarm.service.js with the following:

---
+++ services/devicefarm.service.js
@@ -0,0 +1,47 @@
+const AWS = require("aws-sdk")
+
+class AwsDeviceFarmService {
+  
+  constructor() {
+    this.projectArn = process.env.TEST_GRID_PROJECT
+    this.devicefarm = new AWS.DeviceFarm({ region: "us-west-2" })
+  }
+  
+  // Get a test grid URL once for all test suites / test cases during this wdio testrunner invocation
+  onPrepare(config, capabilities) {
+    return new Promise((resolve, reject) => this.devicefarm.createTestGridUrl({
+      projectArn: this.projectArn,
+      expiresInSeconds: 300
+    }, (err, createTestGridUrlResult) => {
+      if (err) {
+        return reject(err)
+      }
+      
+      console.log("CreateTestGridUrlResult:", createTestGridUrlResult)
+      process.env.AWS_DEVICE_FARM_SERVICE_TEST_GRID_URL = createTestGridUrlResult.url
+      
+      resolve()
+    }))
+  }
+  
+  // Set the connection settings on the beforeSession hook. See more: https://github.com/webdriverio/webdriverio/issues/5113
+  beforeSession(config, capabilities, specs) {
+    return new Promise((resolve, reject) => {
+      
+      // Set the default connection timeout to at least 3 minutes
+      config.connectionRetryTimeout = 180000 // millis
+      
+      const testGridUrl = new URL(process.env.AWS_DEVICE_FARM_SERVICE_TEST_GRID_URL)
+      
+      config.protocol = 'https'
+      config.hostname = testGridUrl.host
+      config.path = testGridUrl.pathname
+      config.port = 443
+      
+      resolve()
+    })
+  }
+  
+}
+
+module.exports = AwsDeviceFarmService
